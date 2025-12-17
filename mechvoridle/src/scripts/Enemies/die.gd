1d12d5f35ae44869d1a5b9ad2404ef627d896b17
DennysGuy <mattmatriciano@gmail.com>
2025-12-16 17:07:20 -0800

    Update die.gd

--- mechvoridle/src/scripts/Enemies/die.gd
+++ mechvoridle/src/scripts/Enemies/die.gd
@@ -3,6 +3,13 @@ class_name EnemyDead extends State
 @export var idle : State 
 func enter() -> void:
 	GameManager.update_score(parent.score)
+	
+	if parent is SwordBot and !parent.is_slave:
+		if parent.is_slave:
+			SignalBus.slave_to_idle.emit()
+	else:
+		SignalBus.free_slave.emit()
+	
 	SignalBus.spawn_health_crate.emit(parent.drop_chance)
 	if parent.hit_flash_animation_player:
 		parent.hit_flash_animation_player.play("Death")
