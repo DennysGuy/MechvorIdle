8d14829c82a7734c5ef0b2fc5c7e5c503fabb41b
Matt Matriciano <mattmatriciano@gmail.com>
2025-12-18 08:41:13 -0800

    getting tutorial panels up.

--- mechvoridle/src/scripts/IntroStuff/tutorial_panel.gd
+++ mechvoridle/src/scripts/IntroStuff/tutorial_panel.gd
@@ -1,9 +1,17 @@
 extends Control
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var tutorial_panels: Control = $TutorialPanels
+
+@onready var basic_movement_pane: ColorRect = $TutorialPanels/BasicMovementPane
+@onready var active_cool_down_attacks_pane: ColorRect = $TutorialPanels/ActiveCoolDownAttacksPane
+@onready var vulcan_machine_gun_pane: ColorRect = $TutorialPanels/VulcanMachineGunPane
+@onready var counter_guard_pane: ColorRect = $TutorialPanels/CounterGuardPane
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	hide_tutorial_panes()
 	animation_player.play("Fade_In")
 
 
@@ -17,19 +25,19 @@ func _on_controller_mappings_button_up() -> void:
 
 
 func _on_basic_movement_button_down() -> void:
-	pass # Replace with function body.
+	basic_movement_pane.show()
 
 
 func _on_attacking_button_down() -> void:
-	pass # Replace with function body.
+	active_cool_down_attacks_pane.show()
 
 
 func _on_vulcan_machine_gun_button_down() -> void:
-	pass # Replace with function body.
+	vulcan_machine_gun_pane.show()
 
 
 func _on_counter_guard_button_down() -> void:
-	pass # Replace with function body.
+	counter_guard_pane.show()
 
 
 func _on_heat_button_up() -> void:
@@ -56,3 +64,44 @@ func go_to_combat() -> void:
 
 func _on_start_game_button_up() -> void:
 	animation_player.play("Fade_Out")
+
+
+func _on_start_movment_tutorial_button_up() -> void:
+	pass # Replace with function body.
+
+	
+func hide_tutorial_panes() -> void:
+	for panel in tutorial_panels.get_children():
+		panel.hide()
+
+
+func _on_start_movement_tutorial_button_up() -> void:
+	pass # Replace with function body.
+
+
+func _on_back_from_movement_tutorial_button_up() -> void:
+	basic_movement_pane.hide()
+
+
+func _on_start_attacks_tutorial_button_up() -> void:
+	pass # Replace with function body.
+
+
+func _on_back_from_cooldown_attacks_button_up() -> void:
+	active_cool_down_attacks_pane.hide()
+
+
+func _on_start_vulcan_machine_gun_tutorial_button_up() -> void:
+	pass # Replace with function body.
+
+
+func _on_back_from_vulcan_machine_gun_button_up() -> void:
+	vulcan_machine_gun_pane.hide()
+
+
+func _on_start_counter_guarding_tutorial_button_up() -> void:
+	pass # Replace with function body.
+
+
+func _on_back_from_counter_guarding_button_up() -> void:
+	counter_guard_pane.hide()
